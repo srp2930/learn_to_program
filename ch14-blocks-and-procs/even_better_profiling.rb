00624936a2a7ac0d21f46e2da3a59764176d3535
srp2930 <s.rimingtonpounder@gmail.com>
2016-05-18 12:56:53 +0100

    Third commit

--- ch14-blocks-and-procs/even_better_profiling.rb
+++ ch14-blocks-and-procs/even_better_profiling.rb
@@ -1,3 +1,32 @@
 def profile block_description, &block
-  # your code here
+	turn_on = false
+	if turn_on
+		start_time = Time.new
+		block.call
+		duration = Time.new - start_time
+		puts "#{block_description}: #{duration} seconds"
+	else
+	block.call
+	end
 end
+
+profile '25000 doublings' do
+	number = 1
+	
+	25000.times do
+	  number = number + number
+	end
+	
+	profile 'printing' do  
+		puts "#{number.to_s.length} digits"
+	end
+	# That's the number of digits in this HUGE number. 
+end
+
+profile 'count to a million' do 
+	number = 0
+	1000000.times do
+  	  number = number + 1
+	end 
+end
+
